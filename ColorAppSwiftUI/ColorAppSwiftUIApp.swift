dc616907ac810b0bb13cf87ec1dc7d03f40edc8a
Alexey Parfenov <aiparfenov88@gmail.com>
2022-01-26 19:12:57 +0300

    Initial Commit

---
+++ ColorAppSwiftUI/ColorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppSwiftUIApp.swift
+//  ColorAppSwiftUI
+//
+//  Created by Алексей Парфенов on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
