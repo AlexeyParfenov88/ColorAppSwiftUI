4ae486243e60deb690e4a4226431f41dbb78d4da
Alexey Parfenov <aiparfenov88@gmail.com>
2022-01-29 17:16:02 +0300

    ContentView added

--- ColorAppSwiftUI/ColorSliderView.swift
+++ ColorAppSwiftUI/ColorSliderView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct ColorSliderView: View {
+    
+    @Binding var value: Double
+    @State private var text = ""
+    
+    let color: Color
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            TextView(value: value)
+            
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(color)
+                .onChange(of: value) { newValue in
+                    text = "\(lround(newValue))"
+                }
+            
+            TextFieldView(text: $text, value: $value)
+        }
+        .onAppear {
+            text = "\(lround(value))"
+        }
     }
 }
 
 struct ColorSliderView_Previews: PreviewProvider {
     static var previews: some View {
-        ColorSliderView()
+        ZStack {
+            Color.gray
+            ColorSliderView(value: .constant(100), color: .red)
+        }
     }
 }
--- ColorAppSwiftUI/ContentView.swift
+++ ColorAppSwiftUI/ContentView.swift
@@ -9,11 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
-    
+    @State private var red = Double.random(in: 0...255)
+    @State private var green = Double.random(in: 0...255)
+    @State private var blue = Double.random(in: 0...255)
+
+//    @FocusState private var isInputActive: Bool
+
     var body: some View {
-        Color(.orange)
-            .ignoresSafeArea()
+        ZStack {
+            Color(.orange)
+                .ignoresSafeArea()
+//                .onTapGesture {
+//                    isInputActive = false
+//                }
+            
+            VStack(spacing: 40) {
+                ColorView(red: red, green: green, blue: blue)
+
+            VStack {
+                ColorSliderView(value: $red, color: .red)
+                ColorSliderView(value: $green, color: .green)
+                ColorSliderView(value: $blue, color: .blue)
+            }
+            .frame(height: 150)
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
